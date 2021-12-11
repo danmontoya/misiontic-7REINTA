9f03acc3de84df2ea59b5ecd3dc0321752ded01b
jhoanSV <jhoanjssv97@outlook.com>
2021-12-11 17:03:53 -0500

    frontend de la inscripción a un nuevo proyecto.

--- frontend/src/components/insproyecto.jsx
+++ frontend/src/components/insproyecto.jsx
@@ -1,53 +1,82 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 export const insproyecto = () => {
     return (
         <div>
-            <div className="container-fluid">
-                <label htmlFor="nuevoSercicio" className="form-label">ID:</label>
-                    <input
-                    type="text"
-                    placeholder="ID del proyecto"
-                    ></input>
-                <label htmlFor="nuevoSercicio" className="form-label">Nombre del proyecto:</label>
-                    <input
-                    type="text"
-                    placeholder="Nombre del proyecto al que se requiere unir"
-                    ></input>
-                <label htmlFor="nuevoSercicio" className="form-label">Presupuesto:</label>
-                    <input
-                    type="number"
-                    placeholder="presupuesto"
-                    ></input>
-                <label htmlFor="nuevoSercicio" className="form-label">ID lider:</label>
-                    <input
-                    type="number"
-                    placeholder="ID del lider"
-                    ></input>
-                <label htmlFor="nuevoSercicio" className="form-label">Nombre lider:</label>
-                    <input
-                    type="number"
-                    placeholder="Nombre del lider"
-                    ></input>
-                <label htmlFor="nuevoSercicio" className="form-label">Objetivos generales:</label>
-                    <input
-                    type="number"
-                    placeholder="Objetivos generales"
-                    ></input>
-                <label htmlFor="nuevoSercicio" className="form-label">Objetivos generales:</label>
-                    <input
-                    type="number"
-                    placeholder="Objetivos generales"
-                    ></input>
-                <label htmlFor="nuevoSercicio" className="form-label">Objetivos especificos:</label>
-                    <input
-                    type="number"
-                    placeholder="Objetivos especificos"
-                    ></input>
-                <label htmlFor="nuevoSercicio" className="form-label">Fecha de inicio:</label>
-                    <input
-                    type="date"
-                    ></input>   
+            <div className="container justify-content align-items-center flex-column">
+                <div className="mt-3">
+                    <div className="row">
+                        <div className="offset-md-2 col-md-9">
+                            <div className="card-title h3 mt-2 ms-2">
+                                Inscripcion a un nuevo proyecto
+                            </div>
+                            <div className="card-body">
+                                <form>
+                                    <label htmlFor="nuevoSercicio" className="form-label">ID:</label>
+                                        <input
+                                        type="text"
+                                        placeholder="ID del proyecto"
+                                        className="form-control"
+                                        ></input>
+                                    <label htmlFor="nuevoSercicio" className="form-label">Nombre del proyecto:</label>
+                                        <input
+                                        type="text"
+                                        placeholder="Nombre del proyecto al que se requiere unir"
+                                        className="form-control"
+                                        ></input>
+                                    <label htmlFor="nuevoSercicio" className="form-label">Presupuesto:</label>
+                                        <input
+                                        type="number"
+                                        placeholder="presupuesto"
+                                        className="form-control"
+                                        ></input>
+                                    <label htmlFor="nuevoSercicio" className="form-label">ID lider:</label>
+                                        <input
+                                        type="number"
+                                        placeholder="ID del lider"
+                                        className="form-control"
+                                        ></input>
+                                    <label htmlFor="nuevoSercicio" className="form-label">Nombre lider:</label>
+                                        <input
+                                        type="text"
+                                        placeholder="Nombre del lider"
+                                        className="form-control"
+                                        ></input>
+                                    <label htmlFor="nuevoSercicio" className="form-label">Objetivos generales:</label>
+                                        <input
+                                        type="text"
+                                        placeholder="Objetivos generales"
+                                        className="form-control"
+                                        ></input>
+                                    <label htmlFor="nuevoSercicio" className="form-label">Objetivos especificos:</label>
+                                        <input
+                                        type="text"
+                                        placeholder="Objetivos especificos"
+                                        className="form-control"
+                                        ></input>
+                                    <div>
+                                    <label htmlFor="nuevoSercicio" className="form-label">Fecha de inicio:</label>
+                                        <input
+                                        type="date"
+                                        ></input>
+                                    </div>
+                                    <div>
+                                    <label htmlFor="nuevoSercicio" className="form-label">Fecha Terminación:</label>
+                                        <input
+                                        type="date"
+                                        ></input>
+                                    </div>
+                                    <div>
+                                    <button type="button" class="btn btn-outline-danger">limpiar</button>
+                                    <button type="button" class="btn btn-outline-primary">actualizar</button>
+                                    <button type="button" class="btn btn-outline-success">Guardar</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
